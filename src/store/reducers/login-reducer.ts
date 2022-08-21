241100e9456b9c80e02d5a88c5eaca52d88c0513
Андрей Ашмянец <andreka6033@gmail.com>
2022-08-21 23:45:32 +0300

    Create login-reducer.ts

---
+++ src/store/reducers/login-reducer.ts
@@ -0,0 +1,9 @@
+const initialState: StateType = {}
+
+export const loginReducer = (state: StateType = initialState, action: ActionsType) => {
+   return state;
+}
+
+type StateType = {}
+
+type ActionsType = {}
