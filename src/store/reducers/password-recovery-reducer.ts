e460d2c446fb81d30583a903221fda6ca7a258c1
Андрей Ашмянец <andreka6033@gmail.com>
2022-08-21 23:45:40 +0300

    Create password-recovery-reducer.ts

---
+++ src/store/reducers/password-recovery-reducer.ts
@@ -0,0 +1,9 @@
+const initialState: StateType = {}
+
+export const passwordRecoveryReducer = (state: StateType = initialState, action: ActionsType) => {
+   return state;
+}
+
+type StateType = {}
+
+type ActionsType = {}
