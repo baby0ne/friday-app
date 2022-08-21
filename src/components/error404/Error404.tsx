7e54ed87b70d886bbfc04446914837fc4db54f1b
Андрей Ашмянец <andreka6033@gmail.com>
2022-08-21 23:44:17 +0300

    Create Error404.tsx

---
+++ src/components/error404/Error404.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+type PropsType = {
+
+}
+
+export const Error404: React.FC<PropsType> = (props) => {
+   const {
+
+   } = props;
+
+   return <>
+      <p>404</p>
+   </>
+}
