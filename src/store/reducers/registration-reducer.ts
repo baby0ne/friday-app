0f3d884ebcd0662e5509f8d5e272d55fc84f8cbe
Андрей Ашмянец <andreka6033@gmail.com>
2022-08-21 23:45:51 +0300

    Create registration-reducer.ts

---
+++ src/store/reducers/registration-reducer.ts
@@ -0,0 +1,9 @@
+const initialState: StateType = {}
+
+export const registrationReducer = (state: StateType = initialState, action: ActionsType) => {
+   return state;
+}
+
+type StateType = {}
+
+type ActionsType = {}
