0f4ef61d21d743419a8515023cca06c19ebb49b2
Андрей Ашмянец <andreka6033@gmail.com>
2022-08-21 23:44:50 +0300

    Create Profile.tsx

---
+++ src/components/profile/Profile.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+type PropsType = {
+
+}
+
+export const Profile: React.FC<PropsType> = (props) => {
+   const {
+
+   } = props;
+
+   return <>
+      <p>Profile</p>
+   </>
+}
