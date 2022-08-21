184973c5182279fd93edc59d238fb5713ed3b5ae
Андрей Ашмянец <andreka6033@gmail.com>
2022-08-21 23:45:36 +0300

    Create new-password-reducer.ts

---
+++ src/store/reducers/new-password-reducer.ts
@@ -0,0 +1,9 @@
+const initialState: StateType = {}
+
+export const newPasswordReducer = (state: StateType = initialState, action: ActionsType) => {
+   return state;
+}
+
+type StateType = {}
+
+type ActionsType = {}
