b8eda48ea92a9a6d45fa1e7ab7a58f2ab9e689c0
Андрей Ашмянец <andreka6033@gmail.com>
2022-08-21 23:44:25 +0300

    Create Login.tsx

---
+++ src/components/login/Login.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+type PropsType = {
+
+}
+
+export const Login: React.FC<PropsType> = (props) => {
+   const {
+
+   } = props;
+
+   return <>
+      <p>Login</p>
+   </>
+}
