91bc01f05adf773e1b273a8cbcff4d5eb722ac41
Андрей Ашмянец <andreka6033@gmail.com>
2022-08-21 23:44:59 +0300

    Create Registration.tsx

---
+++ src/components/registration/Registration.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+type PropsType = {
+
+}
+
+export const Registration: React.FC<PropsType> = (props) => {
+   const {
+
+   } = props;
+
+   return <>
+      <p>Registration</p>
+   </>
+}
