7b8ab8622d6ed7d8b7130883b11cc7799b673a49
Андрей Ашмянец <andreka6033@gmail.com>
2022-08-21 23:45:47 +0300

    Create profile-reducer.ts

---
+++ src/store/reducers/profile-reducer.ts
@@ -0,0 +1,9 @@
+const initialState: StateType = {}
+
+export const profileReducer = (state: StateType = initialState, action: ActionsType) => {
+   return state;
+}
+
+type StateType = {}
+
+type ActionsType = {}
