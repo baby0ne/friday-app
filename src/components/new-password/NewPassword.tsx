ce922d7f3e8b1186753955cfcf62e371b43c496d
Андрей Ашмянец <andreka6033@gmail.com>
2022-08-21 23:44:34 +0300

    Create NewPassword.tsx

---
+++ src/components/new-password/NewPassword.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+type PropsType = {
+
+}
+
+export const NewPassword: React.FC<PropsType> = (props) => {
+   const {
+
+   } = props;
+
+   return <>
+      <p>New password</p>
+   </>
+}
