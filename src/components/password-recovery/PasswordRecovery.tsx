65f70e996168740b36e0e005a25a8bf32a51106b
Андрей Ашмянец <andreka6033@gmail.com>
2022-08-21 23:44:42 +0300

    Create PasswordRecovery.tsx

---
+++ src/components/password-recovery/PasswordRecovery.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+type PropsType = {
+
+}
+
+export const PasswordRecovery: React.FC<PropsType> = (props) => {
+   const {
+
+   } = props;
+
+   return <>
+      <p>Password recovery</p>
+   </>
+}
