d0b552c4a42dbe5b2f61a95d9a5c6fb24cf4c106
Андрей Ашмянец <andreka6033@gmail.com>
2022-08-23 18:31:47 +0300

    Update SuperCheckbox.tsx

--- src/super-components/SuperCheckbox/SuperCheckbox.tsx
+++ src/super-components/SuperCheckbox/SuperCheckbox.tsx
@@ -34,7 +34,7 @@ const SuperCheckbox: React.FC<SuperCheckboxPropsType> = (
             type={'checkbox'}
             onChange={onChangeCallback}
             className={finalInputClassName}
-      
+
             {...restProps} // отдаём инпуту остальные пропсы если они есть (checked например там внутри)
          />
          {children && <span className={s.spanClassName}>{children}</span>}
